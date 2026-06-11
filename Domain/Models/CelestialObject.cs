5ac530bcf1cdb859ed6f9cff48cc85306d28cd37
Mihai Florin Mazureac <Florin.Mazureac@endava.com>
2026-06-11 13:39:19 +0300

    Initial commit and push

---
+++ Domain/Models/CelestialObject.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Models
+{
+    public class CelestialObject
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Mass { get; set; }
+
+        public decimal EquatorialDiameter { get; set; }
+
+        public decimal SurfaceTemperature { get; set; }
+
+        public DateTime DiscoveryDate { get; set; }
+
+        public Guid DiscoverySourceId { get; set; }
+
+        public DiscoverySource DiscoverySource { get; set; }
+
+        public CelestialObjectType Type { get; set; }
+    }
+}
